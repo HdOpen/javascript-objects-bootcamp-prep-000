20d47d3e4a75c48266733c8e81446e6053052d7d
Florin <furtuna.florin90@gmail.com>
2019-01-30 13:54:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = ({ Lorde: "royal"});
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist =  ;
-  return playlist ;
+return playlist.assign ({}, playlist, {[key]: Summer})
+  
 }
